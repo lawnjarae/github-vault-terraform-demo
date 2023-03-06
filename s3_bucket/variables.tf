90750c0aaee6e0495ad6e055085e4b01d139d4dc
Justin Jarae <lawnjarae@gmail.com>
2023-03-06 09:16:06 -0700

    Remove unneedd variables

--- s3_bucket/variables.tf
+++ s3_bucket/variables.tf
@@ -10,12 +10,6 @@ variable "acl" {
   default     = null
 }
 
-variable "policy" {
-  description = "(Optional) A valid bucket policy JSON document. Note that if the policy document is not specific enough (but still valid), Terraform may view the policy as constantly changing in a terraform plan. In this case, please make sure you use the verbose/specific version of the policy. For more information about building AWS IAM policy documents with Terraform, see the AWS IAM Policy Document Guide."
-  type        = string
-  default     = null
-}
-
 variable "tags" {
   description = "(Optional) A mapping of tags to assign to the bucket."
   type        = map(string)
