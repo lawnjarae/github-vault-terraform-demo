a33b3b203a4bd89db5beb2a9f301d857376bb11a
Justin Jarae <lawnjarae@gmail.com>
2023-03-05 20:30:37 -0700

    Perform run on TF cloud

--- s3_bucket/versions.tf
+++ s3_bucket/versions.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+    organization = "hc-field-technology-office"
+
+    workspaces {
+      name = "github-demo"
+    }
+  }
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
